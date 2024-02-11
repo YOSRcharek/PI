bbf0675984ab968cb7428091a8961677096a0bfd
YOSRcharek <yosryassoura31@gmail.com>
2024-02-11 20:46:56 +0100

    dashbored

--- config/bundles.php
+++ config/bundles.php
@@ -2,12 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-<<<<<<< HEAD
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-=======
->>>>>>> d860af511bcfbc6789a840331b4aa628c72bb63b
 ];
---
+++ src/Controller/AdminController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+   
+        return $this->render('baseAdmin.html.twig');
+    }
+    
+    public function buttons(): Response
+    {
+        return $this->render('/admin/buttons.html.twig');
+    }
+    public function cards(): Response
+    {
+        return $this->render('/admin/cards.html.twig');
+    }
+    public function charts(): Response
+    {
+        return $this->render('/admin/charts.html.twig');
+    }
+    public function forms(): Response
+    {
+        return $this->render('/admin/forms.html.twig');
+    }
+    public function modals(): Response
+    {
+        return $this->render('/admin/modals.html.twig');
+    }
+    public function tables(): Response
+    {
+        return $this->render('/admin/tables.html.twig');
+    }
+    public function erreur(): Response
+    {
+        return $this->render('/admin/pages/404.html.twig');
+    }
+    public function blank(): Response
+    {
+        return $this->render('/admin/pages/blank.html.twig');
+    }
+    public function createAcc(): Response
+    {
+        return $this->render('/admin/pages/create-account.html.twig');
+    }
+    public function forgetPass(): Response
+    {
+        return $this->render('/admin/pages/forget-password.html.twig');
+    }
+    public function login(): Response
+    {
+        return $this->render('/admin/pages/login.html.twig');
+    }
+}
