1522184725eb028e351afa51a7489b3ac3502405
walid.khrouf@esprit.tn <100620415+walidkhrouf@users.noreply.github.com>
2024-03-05 01:51:28 +0100

    adding apis with crud functions

--- src/Controller/DonsController.php
+++ src/Controller/DonsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Dons;
 use App\Form\DonsType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,17 +11,30 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Repository\DonsRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Stripe\Stripe;
+use Stripe\Exception\ApiErrorException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
 
 class DonsController extends AbstractController
 {
     #[Route('/dons', name: 'app_dons')]
-    public function show(DonsRepository $DonsRepository): Response
+    public function show(DonsRepository $DonsRepository, Request $request, PaginatorInterface $paginator): Response
     {
-        $Dons = $DonsRepository->findAll();
+        
+
+        $pagination = $paginator->paginate(
+            $DonsRepository->paginationQuery(),
+            $request->query->getInt('page',1),
+            6
+        );
+
         return $this->render('dons/show.html.twig', [
-            'Dons' => $Dons,
+            'pagination' => $pagination,
         ]);
     }
+
     #[Route('/donsadmin', name: 'app_dons_admin')]
     public function shows(DonsRepository $DonsRepository): Response
     {
@@ -29,43 +43,47 @@ class DonsController extends AbstractController
             'Dons' => $Dons,
         ]);
     }
+
     #[Route('/donate', name: 'dons_add')]
-    public function add(Request $request): Response
+    public function add(Request $request, ManagerRegistry $doctrine): Response
     {
         $dons = new Dons();
-    
         $form = $this->createForm(DonsType::class, $dons);
         $form->handleRequest($request);
-        try{
+    
+        try {
             if ($form->isSubmitted() && $form->isValid()) {
-                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager = $doctrine()->getManager();
                 $entityManager->persist($dons);
                 $entityManager->flush();
     
+                $montant = $dons->getMontant();
+    
                 $this->addFlash('success', 'Don ajouté avec succès.');
     
-                return $this->redirectToRoute('app_dons');
+                // Redirect to the index route with the montant parameter
+                return $this->redirectToRoute('app_stripe', ['montant' => $montant]);
             }
-        }catch (\Exception $e) {
+        } catch (\Exception $e) {
             $this->addFlash('error', 'Une erreur s\'est produite lors de l\'ajout d\'un don .');
         }
+    
         return $this->render('home/donate.html.twig', [
             'form' => $form->createView(),
         ]);
     }
-
-
+    
 
     #[Route('/donateadmin', name: 'dons_add_admin')]
-    public function adds(Request $request): Response
+    public function adds(Request $request, ManagerRegistry $doctrine): Response
     {
         $dons = new Dons();
     
         $form = $this->createForm(DonsType::class, $dons);
         $form->handleRequest($request);
-        try{
+        try {
             if ($form->isSubmitted() && $form->isValid()) {
-                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager = $doctrine()->getManager();
                 $entityManager->persist($dons);
                 $entityManager->flush();
     
@@ -73,7 +91,7 @@ class DonsController extends AbstractController
     
                 return $this->redirectToRoute('app_dons_admin');
             }
-        }catch (\Exception $e) {
+        } catch (\Exception $e) {
             $this->addFlash('error', 'Une erreur s\'est produite lors de l\'ajout d\'un don .');
         }
         return $this->render('admin/adddonsadmin.html.twig', [
@@ -102,7 +120,6 @@ class DonsController extends AbstractController
         ]);
     }
 
-
     #[Route('/modifierDonsAdmin/{id}', name: 'dons_modifier_admin', methods: ['GET', 'POST'])]
     public function modifieradmin(Request $request, EntityManagerInterface $entityManager, $id, DonsRepository $donsRepository): Response
     {
@@ -123,7 +140,6 @@ class DonsController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
-    
 
     #[Route('/deleteDons/{id}', name: 'dons_delete')]
     public function deleteDons(Request $request, $id, EntityManagerInterface $entityManager, DonsRepository $donsRepository): Response
@@ -142,7 +158,6 @@ class DonsController extends AbstractController
         return $this->redirectToRoute('app_dons');
     }
 
-
     #[Route('/deleteDonsAdmin/{id}', name: 'dons_delete_admin')]
     public function deleteDonsAdmin(Request $request, $id, EntityManagerInterface $entityManager, DonsRepository $donsRepository): Response
     {
@@ -159,9 +174,66 @@ class DonsController extends AbstractController
     
         return $this->redirectToRoute('app_dons_admin');
     }
+
+    #[Route('/stripe', name: 'app_stripe')]
+    public function index(Request $request): Response
+    {
+        
+        $montant = $request->query->get('montant') *100;
+    
+        return $this->render('stripe/index.html.twig', [
+            'stripe_key' => $_ENV["STRIPE_KEY"],
+            'montant' => $montant,
+        ]);
+    }
     
 
+    #[Route('/stripe/create-charge', name: 'app_stripe_charge', methods: ['POST'])]
+    public function createCharge(Request $request): Response
+    {
+    
+        $montant = $request->request->get('montant');
+    
+        try {
+            \Stripe\Stripe::setApiKey($_ENV["STRIPE_SECRET"]);
+            \Stripe\Charge::create([
+                "amount" => $montant,
+                "currency" => "usd",
+                "source" => $request->request->get('stripeToken'),
+                "description" => "Binaryboxtuts Payment Test"
+            ]);
+    
+            $this->addFlash('success', 'Payment Successful!');
+        } catch (ApiErrorException $e) {
+            $this->addFlash('error', 'An error occurred while processing the payment.');
+        }
+    
+        return $this->redirectToRoute('app_dons', [], Response::HTTP_SEE_OTHER);
+    }
 
 
 
+    #[Route('/filter-dons-by-montant', name: 'filter_dons_by_montant')]
+    public function filterDonsByMontant(Request $request, DonsRepository $donsRepository): JsonResponse
+    {
+        $montant = $request->query->get('montant');
+    
+        $dons = $donsRepository->findByMontant($montant);
+    
+        // Serialize the entities to an array
+        $donsArray = [];
+        foreach ($dons as $don) {
+            $donsArray[] = [
+                'id' => $don->getId(),
+                'montant' => $don->getMontant(),
+                'dateMisDon' => $don->getDateMisDon()->format('d-m-Y'),
+                'association' => $don->getAssociation() !== null ? $don->getAssociation()->getNom() : null,
+                'type' => $don->getType() !== null ? $don->getType()->getNom() : null,
+            ];
+        }
+    
+        // Return the serialized entities as JSON response
+        return new JsonResponse(['dons' => $donsArray]);
+    }
 }
+
